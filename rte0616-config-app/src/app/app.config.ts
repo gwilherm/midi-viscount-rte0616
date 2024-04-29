1efd0425c595793c2894c702a39fa3d9d3e40c5d
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-04-30 00:10:21 +0200

    Fix sysex support

--- rte0616-config-app/src/app/app.config.ts
+++ rte0616-config-app/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SYSEX } from '@ng-web-apis/midi';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: SYSEX,
+      useValue: true
+    }
+  ]
 };
