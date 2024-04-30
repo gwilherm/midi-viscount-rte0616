77a715926e41555dd50f489b14af075dcded72c8
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-04-30 20:20:32 +0200

    Add device component

--- rte0616-config-app/src/app/app.component.ts
+++ rte0616-config-app/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, Inject, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MIDI_INPUTS, MIDI_OUTPUTS } from '@ng-web-apis/midi';
-import { Observable } from 'rxjs';
+import { DevicesComponent } from './devices/devices.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DevicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,25 +13,9 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'rte0616-config-app';
 
-  constructor(
-    @Inject(MIDI_INPUTS) inputs: Observable<MIDIInput[]>,
-    @Inject(MIDI_OUTPUTS) outputs: Observable<MIDIOutput[]>,
-  )
+  constructor()
   {
-    inputs.subscribe({
-      next: inputs => { inputs.filter(i => i.name?.startsWith('Viscount RTE0616'))[0] }
-    })
 
-    outputs.subscribe({
-      next: outputs => { 
-        console.log(outputs)
-        const output = outputs.filter(o => o.name?.startsWith('Viscount RTE0616'))[0]
-        if (output) {
-          console.log(output.name)
-          output.send([0xF0, 0x7E, 0x7F, 0x06, 0x01, 0xF7])
-        }
-      }
-    })
   }
   
   ngOnInit(): void {
---
+++ rte0616-config-app/src/app/devices/devices.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MIDI_INPUTS, MIDI_OUTPUTS } from '@ng-web-apis/midi';
+import { Observable } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-devices',
+  standalone: true,
+  imports: [NgIf, NgFor, AsyncPipe, MatSelectModule],
+  templateUrl: './devices.component.html',
+  styleUrl: './devices.component.css'
+})
+export class DevicesComponent implements OnInit {
+  inputs$: Observable<MIDIInput[]>;
+  outputs$: Observable<MIDIOutput[]>;
+  selectedDevice: MIDIOutput | undefined;
+  constructor(
+    @Inject(MIDI_INPUTS) inputs$: Observable<MIDIInput[]>,
+    @Inject(MIDI_OUTPUTS) outputs$: Observable<MIDIOutput[]>,
+  )
+  {
+    this.inputs$ = inputs$
+    this.outputs$ = outputs$
+
+    // outputs.subscribe({
+    //   next: outputs => { 
+    //     console.log(outputs)
+    //     const output = outputs.filter(o => o.name?.startsWith('Viscount RTE0616'))[0]
+    //     if (output) {
+    //       console.log(output.name)
+    //       output.send([0xF0, 0x7E, 0x7F, 0x06, 0x01, 0xF7])
+    //     }
+    //   }
+    // })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  selectDevice(ev: any, dev: MIDIOutput) {
+    this.selectedDevice = dev;
+  }
+
+  canCheck(): boolean {
+    return this.selectedDevice != undefined;
+  }
+}
