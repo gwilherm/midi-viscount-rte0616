fc4f6377aa592f8976868ed7ca0d033a20ca1c3f
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-04-30 20:20:11 +0200

    ng add @angular/material

--- rte0616-config-app/src/app/app.config.ts
+++ rte0616-config-app/src/app/app.config.ts
@@ -4,12 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { SYSEX } from '@ng-web-apis/midi';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     {
       provide: SYSEX,
       useValue: true
-    }
+    },
+    provideAnimations(), provideAnimationsAsync()
   ]
 };
