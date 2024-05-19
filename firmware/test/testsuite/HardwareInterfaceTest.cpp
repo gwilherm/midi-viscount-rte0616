bcf54523c2bee8c20efe6e4bc5a664970cdf23dd
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-05-19 13:56:07 +0200

    simple HardwareInterface.loop unit test

--- firmware/test/testsuite/HardwareInterfaceTest.cpp
+++ firmware/test/testsuite/HardwareInterfaceTest.cpp
@@ -21,6 +21,7 @@ protected:
 
 TEST_F(HardwareInterfaceTest, setup)
 {
+    // All analog pins are pulled up
     EXPECT_CALL(ArduinoMock::getInstance(), pinMode(A0, INPUT_PULLUP));
     EXPECT_CALL(ArduinoMock::getInstance(), pinMode(A1, INPUT_PULLUP));
     EXPECT_CALL(ArduinoMock::getInstance(), pinMode(A2, INPUT_PULLUP));
@@ -33,3 +34,20 @@ TEST_F(HardwareInterfaceTest, setup)
     _hwInterface.setup();
 }
 
+TEST_F(HardwareInterfaceTest, loop_check_calls)
+{
+    // All analog pins are read once
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A0));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A1));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A2));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A3));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A6));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A7));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A8));
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(A9));
+
+    // Each pin is debounced
+    EXPECT_CALL(ArduinoMock::getInstance(), millis()).Times(NB_PIN*2);
+
+    _hwInterface.loop();
+}
