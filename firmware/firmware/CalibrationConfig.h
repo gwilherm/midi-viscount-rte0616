0d5a1472a2e779bc846e9568ae9ebf90210791de
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-05-20 21:18:23 +0200

    Bugfix

--- firmware/firmware/CalibrationConfig.h
+++ firmware/firmware/CalibrationConfig.h
@@ -22,12 +22,21 @@ public:
 
     inline uint16_t getMargin() { return _margin; };
     inline void setMargin(uint16_t m) { _margin = m; };
-    inline uint16_t getVSeg(int i) { return ((i >= 0) && (i <= NB_VSEG))? _vSeg[i] : 0; }
-    inline void setVSeg(int i, uint16_t v) { if ((i >= 0) && (i <= NB_VSEG)) _vSeg[i] = v; }
+    inline uint16_t getVSeg(int i) { return ((i >= 0) && (i < NB_VSEG))? _vSeg[i] : 0; }
+    inline void setVSeg(int i, uint16_t v) { if ((i >= 0) && (i < NB_VSEG)) _vSeg[i] = v; }
+
 
 private:
     uint16_t _margin;
     uint16_t _vSeg[NB_VSEG];
 };
 
+void CalibrationConfig::reset()
+{
+    _margin = DEFAULT_MARGIN;
+    _vSeg[0] = DEFAULT_VSEG1;
+    _vSeg[1] = DEFAULT_VSEG2;
+    _vSeg[2] = DEFAULT_VSEG3;
+    _vSeg[3] = DEFAULT_VSEG4;
+}
 #endif
