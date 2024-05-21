978efbd16abcf894c2d8cde90a0b3a1092e02347
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-05-21 19:35:47 +0200

    Config App: Workaround for reload menu

--- rte0616-config-app/main.js
+++ rte0616-config-app/main.js
@@ -6,13 +6,17 @@ let win;
 function createWindow() {
   win = new BrowserWindow({ width: 1050, height: 850 , autoHideMenuBar: true});
   // load the dist folder from Angular
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "/dist/rte0616-config-app/browser/index.html"),
-      protocol: "file:",
-      slashes: true
-    })
-  );
+  const appUrl = url.format({
+    pathname: path.join(__dirname, "/dist/rte0616-config-app/browser/index.html"),
+    protocol: "file:",
+    slashes: true
+  });
+
+  win.loadURL(appUrl);
+
+  // Workaround for reload menu.
+  win.webContents.on('did-fail-load', () => win.loadURL(appUrl));
+
   // The following is optional and will open the DevTools:
   // win.webContents.openDevTools()
   win.on("closed", () => {
