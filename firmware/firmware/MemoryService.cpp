432ea91920f3a602ac941427cfcefb669114a362
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-06-02 12:53:56 +0200

    Overcomplicated

--- firmware/firmware/MemoryService.cpp
+++ firmware/firmware/MemoryService.cpp
@@ -2,58 +2,26 @@
 #include "IEEPROMInterface.h"
 #include "MIDIConfig.h"
 #include "CalibrationConfig.h"
+#include <cstdint>
 
 #define MANUFACTURER_ID 0x31 	// Viscount Manufacturer ID
 #define PRODUCT_ID_MSB  0x06
 #define PRODUCT_ID_LSB  0x16	// for RTE0616
 
-struct device_info_store_s {
-    device_info_store_s() {
-        char* fwVersionStr = strdup(FW_VERSION);
-    	int tmp = atoi(strtok(fwVersionStr, "."));
-        v[0] = tmp & 0xFF;
-    	tmp = atoi(strtok(NULL, "."));
-        v[1] = tmp & 0xFF;
-        tmp = atoi(strtok(NULL, "."));
-        v[2] = (tmp >> 8) & 0xFF;
-        v[3] = tmp & 0xFF;
-    };
-    uint8_t m = MANUFACTURER_ID;
-    uint8_t p[2] = {PRODUCT_ID_MSB,PRODUCT_ID_LSB};
-    uint8_t v[4];
-};
-
-typedef union device_info_store_u
-{
-    device_info_store_u() : info() {};
-    struct device_info_store_s info;
-    uint8_t bytes[sizeof(struct device_info_store_s)];
+typedef struct {
+    uint8_t manu_id;
+    uint8_t prod_id[2];
+    uint8_t fw_ver[4];
 } device_info_store_t;
 
-typedef union midi_config_store_u
-{
-    midi_config_store_u() {};
-    midi_config_store_u(MidiConfig& midiConfig) : cfg(midiConfig.getChannel(), midiConfig.getOctave()) {};
-    struct midi_config_store_s {
-        midi_config_store_s(uint8_t c, uint8_t o) : channel(c), octave(o) {};
-        uint8_t channel;
-        uint8_t octave;
-    } cfg;
-    uint8_t bytes[sizeof(struct midi_config_store_s)];
+typedef struct {
+    uint8_t channel;
+    uint8_t octave;
 } midi_config_store_t;
 
-typedef union calibration_store_u
-{
-    calibration_store_u() {};
-    calibration_store_u(CalibrationConfig& calConfig) : cal(calConfig.getMargin()) {
-        for (int i = 0; i < NB_VSEG; i++) cal.vseg[i] = calConfig.getVSeg(i);
-    };
-    struct calibration_store_s {
-        calibration_store_s(uint8_t m) : margin(m) {};
-        uint16_t margin;
-        uint16_t vseg[NB_VSEG];
-    } cal;
-    uint8_t bytes[sizeof(struct calibration_store_s)];
+typedef struct {
+    uint16_t margin;
+    uint16_t vseg[NB_VSEG];
 } calibration_store_t;
 
 MemoryService::MemoryService(IEEPROMInterface& eepromInterface, MidiConfig& midiConfig, CalibrationConfig& calibrationConfig):
@@ -78,10 +46,23 @@ bool MemoryService::readDeviceInfo(int& offset)
 int MemoryService::updateDeviceInfo()
 {
     int offset = 0;
-    device_info_store_t dev;
+    device_info_store_t dev = {
+        .manu_id = MANUFACTURER_ID,
+        .prod_id = { PRODUCT_ID_MSB, PRODUCT_ID_LSB }
+    };
 
+    char* fwVersionStr = strdup(FW_VERSION);
+    int tmp = atoi(strtok(fwVersionStr, "."));
+    dev.fw_ver[0] = tmp & 0xFF;
+    tmp = atoi(strtok(NULL, "."));
+    dev.fw_ver[1] = tmp & 0xFF;
+    tmp = atoi(strtok(NULL, "."));
+    dev.fw_ver[2] = (tmp >> 8) & 0xFF;
+    dev.fw_ver[3] = tmp & 0xFF;
+
+    uint8_t* ptr = (uint8_t*)&dev;
     for (int i = 0; i < sizeof(device_info_store_t); i++)
-        _eepromInterface.update(offset++, dev.bytes[i]);
+        _eepromInterface.update(offset++, *ptr++);
 
     return offset;
 }
@@ -89,10 +70,14 @@ int MemoryService::updateDeviceInfo()
 int MemoryService::updateMidiConfig()
 {
     int offset = sizeof(device_info_store_t);
-    midi_config_store_t cfg(_midiConfig);
+    midi_config_store_t cfg = {
+        .channel = _midiConfig.getChannel(),
+        .octave = _midiConfig.getOctave()
+    };
 
-    for (int i = 0; i < sizeof(midi_config_store_t); i++)
-        _eepromInterface.update(offset++, cfg.bytes[i]);
+    uint8_t* ptr = (uint8_t*)&cfg;
+    for (int i = 0; i < sizeof(cfg); i++)
+        _eepromInterface.update(offset++, *ptr++);
 
     return offset;
 }
@@ -101,43 +86,54 @@ int MemoryService::updateCalibration()
 {
     int offset = 7 + sizeof(midi_config_store_t);
 
-    calibration_store_t cal(_calibrationConfig);
+    calibration_store_t cal = {
+        .margin = _calibrationConfig.getMargin(),
+        .vseg = {
+            _calibrationConfig.getVSeg(0),
+            _calibrationConfig.getVSeg(1),
+            _calibrationConfig.getVSeg(2),
+            _calibrationConfig.getVSeg(3),
+        }
+    };
+
+    uint8_t* ptr = (uint8_t*)&cal;
     for (int i = 0; i < sizeof(calibration_store_t);  i++)
-        _eepromInterface.update(offset++, cal.bytes[i]);
+        _eepromInterface.update(offset++, *ptr++);
 
     return offset;
 }
 
 void MemoryService::store()
 {
-    int offset = 0;
-
-    offset = updateDeviceInfo();
-    offset = updateMidiConfig();
-    offset = updateCalibration();
+    updateDeviceInfo();
+    updateMidiConfig();
+    updateCalibration();
 }
 
 void MemoryService::restore()
 {
     midi_config_store_t cfg;
     calibration_store_t cal;
+    uint8_t* ptr;
 
     int offset = 0;
 
     if (readDeviceInfo(offset))
     {
+        ptr = (uint8_t*)&cfg;
         for (int i = 0; i < sizeof(midi_config_store_t); i++)
-            cfg.bytes[i] = _eepromInterface.read(offset++);
+            *ptr++ = _eepromInterface.read(offset++);
 
+        ptr = (uint8_t*)&cal;
         for (int i = 0; i < sizeof(calibration_store_t);  i++)
-            cal.bytes[i] = _eepromInterface.read(offset++);
+            *ptr++ = _eepromInterface.read(offset++);
 
-        _midiConfig.setChannel(cfg.cfg.channel);
-        _midiConfig.setOctave(cfg.cfg.octave);
+        _midiConfig.setChannel(cfg.channel);
+        _midiConfig.setOctave(cfg.octave);
 
-        _calibrationConfig.setMargin(cal.cal.margin);
+        _calibrationConfig.setMargin(cal.margin);
         for (int i = 0; i < NB_VSEG; i++)
-            _calibrationConfig.setVSeg(i, cal.cal.vseg[i]);
+            _calibrationConfig.setVSeg(i, cal.vseg[i]);
     }
     else {
         factoryReset();
