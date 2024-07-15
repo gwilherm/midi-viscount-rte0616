0c54e31f8ae2a943645bbbc6f35c044c882556bb
Guillaume <1017720+gwilherm@users.noreply.github.com>
2024-07-15 13:07:09 +0200

    Remove useless loop

--- firmware/firmware/MIDINotesService.cpp
+++ firmware/firmware/MIDINotesService.cpp
@@ -4,7 +4,7 @@
 
 MIDINotesService::MIDINotesService(IMIDIInterface& usbMidiInterface, MidiConfig& midiConfig, IHardwareInterface& hwInterface) :
     _usbMidiInterface(usbMidiInterface),
-	_midiConfig(midiConfig),
+    _midiConfig(midiConfig),
     _hwInterface(hwInterface),
     _currentKey(PDLBRD_NO_KEY_PRESSED)
 {}
@@ -14,27 +14,24 @@ void MIDINotesService::loop()
     pdlbrd_key_t newKey = PDLBRD_NO_KEY_PRESSED;
     int8_t* pinSegment = _hwInterface.getSegmentedValues();
 
-	for (uint8_t seg = 0; seg < PDLBRD_NB_SEG; seg++)
-	{
-		for (uint8_t pin = 0; pin < NB_PIN; pin++)
-		{
-			if (pinSegment[pin] != PDLBRD_NO_SEG)
-			{
-				pdlbrd_key_t k = PDLBRD_KEY_MAP[pinSegment[pin]][pin];
-				if ((newKey == PDLBRD_NO_KEY_PRESSED) || (k != PDLBRD_NO_KEY_PRESSED) && (k > newKey))
-					newKey = k;
-			}
-		}
-	}
+    for (uint8_t pin = 0; pin < NB_PIN; pin++)
+    {
+        if (pinSegment[pin] != PDLBRD_NO_SEG)
+        {
+            pdlbrd_key_t k = PDLBRD_KEY_MAP[pinSegment[pin]][pin];
+            if ((newKey == PDLBRD_NO_KEY_PRESSED) || (k != PDLBRD_NO_KEY_PRESSED) && (k > newKey))
+                newKey = k;
+        }
+    }
 #if defined (FW_DEBUG_VERBOSE_RTE0616PEDALBOARD)
     Serial.println(PDLBRD_KEY_STR[newKey]);
 #endif
 
-	if (_currentKey != newKey) {
-		if (_currentKey != PDLBRD_NO_KEY_PRESSED)
-			_usbMidiInterface.sendNoteOff((_midiConfig.getOctave()*TONES_IN_OCTAVE)+_currentKey, _midiConfig.getChannel());
-		if (newKey != PDLBRD_NO_KEY_PRESSED)
-			_usbMidiInterface.sendNoteOn((_midiConfig.getOctave()*TONES_IN_OCTAVE)+newKey, _midiConfig.getChannel());
-		_currentKey = newKey;
-	}
+    if (_currentKey != newKey) {
+        if (_currentKey != PDLBRD_NO_KEY_PRESSED)
+            _usbMidiInterface.sendNoteOff((_midiConfig.getOctave()*TONES_IN_OCTAVE)+_currentKey, _midiConfig.getChannel());
+        if (newKey != PDLBRD_NO_KEY_PRESSED)
+            _usbMidiInterface.sendNoteOn((_midiConfig.getOctave()*TONES_IN_OCTAVE)+newKey, _midiConfig.getChannel());
+        _currentKey = newKey;
+    }
 }
